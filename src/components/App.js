909cec45ff1a2b035b536ef5e8a0378ac698d112
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-09-22 22:52:49 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "./Logo.js";
+import Form from "./Form.js";
+import PackingList from "./PackingList.js";
+import Stats from "./Stats.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PackingList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Item from "./Item.js";
+
+const PackingList = () => {
+  return (
+    <div>
+      Packing List
+      <Item />
+    </div>
+  );
+};
+
+export default PackingList;
